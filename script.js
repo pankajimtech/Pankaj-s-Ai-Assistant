9bd1cf1153fcacb8bd3da3b4536103557df92d1e
Aditi K Chauhan <158515786+pankajimtech@users.noreply.github.com>
2024-12-08 12:56:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,575 @@
+let word = prompt ("ask a question related to Pankaj's reacherch. Write in only LOWERCASE. If you want to know what to search, so please search the word INFORMATION");
+// document.write(weather);
+
+
+
+if (word == "hi"){
+    document.write("Hi! welcome to Pankaj's Assistant! if needed info, please search information");
+}
+
+
+
+if (word == "information"){
+    document.write("you can search: cancer, protease, mmp1, mmp2, mmp3, mmp7, mmp8, mmp9, mmp10, mmp11, mmp12, mmp13, mmp14, mmp15, mmp16, mmp17, mmp18, mmp19, mmp20, mmp21, mmp23b, mmp24, mmp25, mmp26, mmp27, mmp28, immp1l, immp2l, blca, brca, coad, esca, hnsc, kich, kirc, kirp, lihc, luad, lusc, erad, stad, thca, ucec, mmp substrate sequence");
+}
+
+
+
+
+if (word == "pankaj"){
+    document.write("is a monkey.");
+}
+
+
+
+if (word == "protease"){
+    document.write("mmp1, mmp2, mmp3, mmp7, mmp8, mmp9, mmp10, mmp11, mmp12, mmp13, mmp14, mmp15, mmp16, mmp17, mmp18, mmp19, mmp20, mmp21, mmp23b, mmp24, mmp25, mmp26, mmp27, mmp28, immp1l, immp2l");
+}
+
+
+if (word == "blca"){
+    document.write("mmp1[substrate GPQ/D/YA/N/↓L/I/VI/V/L/TG/A/DP/D/GVD/G/P]");
+}
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp1"){
+    document.write("Sequence GPASLGQE");
+}
+
+
+
+if (word == "mmp2"){
+    document.write("Sequence GPASLGQE");
+}
+
+
+
+
+
+if (word == "mmp3"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "mmp7"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp8"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp9"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp10"){
+    document.write("info result here");
+}
+
+
+
+
+
+if (word == "mmp11"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp12"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp13"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp14"){
+    document.write("info result here");
+}
+
+
+
+
+
+if (word == "mmp15"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp16"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp17"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp18"){
+    document.write("info result here");
+}
+
+
+
+
+
+if (word == "mmp19"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp20"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp21"){
+    document.write("info result here");
+}
+
+
+
+if (word == "mmp23b"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
+
+
+
+if (word == "searched"){
+    document.write("info result here");
+}
+
